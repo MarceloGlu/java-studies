350d6db1d2308734e1b90cdec23ca5bbd7d23288
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2021-12-15 09:07:32 +0200

    Name changed to match that of Eldar

--- 14-12-21/src/a/Demo2.java
+++ 14-12-21/src/a/Demo2.java
@@ -4,17 +4,7 @@ public class Demo2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		//int array of length 100
-		int[] grades = new int[100];
-		
-		// populate array with random data  between 60 and 100 and print				
-		for (int i = 0; i < grades.length; i++) {
-			grades[i] = (int) (Math.random() * 41) + 60;
-			System.out.println(grades[i]);
-		}
 
 	}
-	
 
 }
