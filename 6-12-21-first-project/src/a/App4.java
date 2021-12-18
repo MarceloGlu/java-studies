4eceb1413bbb77343b4fc70e8157577723121883
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2021-12-18 15:12:45 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of https://github.com/MarceloGlu/java-course

--- 6-12-21-first-project/src/a/App4.java
+++ 6-12-21-first-project/src/a/App4.java
@@ -3,24 +3,38 @@ package a;
 public class App4 {
 
 	public static void main(String[] args) {
-		
+
 		// input
 		// System.in.read();
 		
+
 		// output
-		System.out.println("This is an ouptut");
+		System.out.println("This is an output.");
 		
-		
-		//assignment
-		int x = 15;
-		int y = 5;
-		System.out.println(x);
-		System.out.println(y);
-		
+		// assignment
+				int x = 15;
+				int y = 5;
+				System.out.println("X = " + x + " and Y = " + y + ".");
+				System.out.println("Is X bigger?");
+				
+
+		// if
+		if (x > y) {
+			System.out.println("Yes, X is bigger.");
+		} else {
+			System.out.println("No, X is not bigger.");
 		}
-		
-		
+
+		// loop
+		System.out.println("For a variable i that goes from 1 to 10, print all its instances.");
+		for (int i = 1; i <= 10; i++) {
+			System.out.print(i + ", ");
+		}
+		System.out.println();
+
+		System.out.println("Well done!");
+		System.out.println("END");
 
 	}
 
-
+}
