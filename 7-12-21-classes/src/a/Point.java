bd09052064a55e7d986e6c78936c220abdbbfb75
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2021-12-19 13:05:51 +0200

    Actualice viendo el rep de Eldar

--- 7-12-21-classes/src/a/Point.java
+++ 7-12-21-classes/src/a/Point.java
@@ -1,11 +1,12 @@
 package a;
 
 public class Point {
-	
+
+	// atributos
 	int x;
 	int y;
-	
-	
+
+	// funcion
 	void print() {
 		System.out.print("(");
 		System.out.print(x);
@@ -13,42 +14,14 @@ public class Point {
 		System.out.print(y);
 		System.out.println(")");
 	}
-		
-	void moveRight(){
+
+	// funcion
+	void moveRight() {
 		x++;
 	}
-	void moveLeft(){
+
+	// funcion
+	void moveLeft() {
 		x--;
 	}
-	
-	
-	/*public static void main(String[] args) {
-		
-		Point p = new Point();
-		p.print();
-		p.x = 25;
-		p.y = 5;
-		p.print();
-		
-		Point p2 = new Point();
-		p2.print();
-		p2.x = 30;
-		p2.y = 60;
-		p2.print();
-	}
-		
-	
-	
-	public static void main(String[] args) {
-		Point pt; //variable declaration: type + identifier
-		point = new Point; //variable initialization;
-		Point pt2 = new Point; //declaration + initialization
-		
-		int x; //declaration
-		x = 25; //initialization
-		
-		int y = 300; //declaration + initialization		
-		
-	}*/
-
 }
--- 7-12-21-classes/src/a/PointTest.java
+++ 7-12-21-classes/src/a/PointTest.java
@@ -1,20 +1,15 @@
 package a;
 
 public class PointTest {
-	
-public static void main(String[] args) {
-		
-		Point p = new Point();
-		p.print();
-		p.x = 25;
-		p.y = 5;
-		p.print();
-		
-		Point p2 = new Point();
-		p2.print();
-		p2.x = 30;
-		p2.y = 60;
-		p2.print();
+
+	public static void main(String[] args) {
+
+		Point p1 = new Point();
+		p1.print();
+		p1.x = 100;
+		p1.y = 200;
+		p1.print();
+
 	}
 
 }
