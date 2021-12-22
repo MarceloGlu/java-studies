f337a080f2c029598dc430b7eaaca83505943f89
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2021-12-22 23:47:44 +0200

    update

--- ejercicios-personales/src/c/loopsandarrays/RandomCount.java
+++ ejercicios-personales/src/c/loopsandarrays/RandomCount.java
@@ -4,8 +4,8 @@ public class RandomCount {
 
 	public static void main(String[] args) {
 
-		// this program generates 100 random values betwee 0 and 9
-		// and them prints how many of them are equals to number 5
+		// this program generates 100 random values between 0 and 9
+		// and then prints how many of them are equals to number 5
 
 		int fiveCounter = 0;
 		for (int i = 0; i < 100; i++) {
