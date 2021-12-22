91831fd5113919380f1311be63491c48e07f10b7
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2021-12-22 07:54:55 +0200

    update

--- Lab1/src/Test1.java
+++ Lab1/src/Test1.java
@@ -3,11 +3,12 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
+		
 		double num = (Math.random() * 250);
 		System.out.println(num + " minutes");
 		
-		int minutes = (num % 60);
-		int hours = (num /60);
+		double minutes = (num % 60);
+		double hours = (num /60);
 		
 		System.out.println("hours: " + hours + "minutes: " + minutes);
 		
