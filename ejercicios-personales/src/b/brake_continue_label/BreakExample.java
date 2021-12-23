6a406cf371eeda62afd937e917ced2ea1039d809
jbt <jbt@405_01>
2021-12-23 09:08:21 +0200

    update

--- ejercicios-personales/src/b/brake_continue_label/BreakExample.java
+++ ejercicios-personales/src/b/brake_continue_label/BreakExample.java
@@ -2,19 +2,23 @@ package b.brake_continue_label;
 
 public class BreakExample {
 	
-	// The BREAK STATEMENT terminates the loop or switch statement and transfers execution
-	// to the statement immediately following the loop or switch.
-	// En este caso la tarea es imprimir de 0 a 9 pero salteando el 8.
-		 
+	
+		
+	
+
+	// The BREAK STATEMENT terminates the loop or switch statement and transfers
+	// execution to the statement immediately following the loop or switch.
+	// En este caso imprime solo hasta el 7 y salta al proximo statement.
 
 	public static void main(String[] args) {
-		
-		for(int i=0; i<10; i++){
-	         if (i==8){
-	            break;
-	         }
-	         System.out.println("......."+i);
-	      }		
+
+		for (int i = 0; i < 10; i++) {
+			if (i == 8) {
+				break;
+			}
+			System.out.println("......." + i);
+	
+		}
 
 	}
 
