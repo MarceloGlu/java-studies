9b382ca22b5ed0473be5514a17010b2a978e96a8
jbt <jbt@405_01>
2021-12-26 09:59:31 +0200

    update

---
+++ ejercicios-personales/src/e/lab2/Smaller.java
@@ -0,0 +1,25 @@
+package e.lab2;
+
+public class Smaller {
+
+	public static void main(String[] args) {
+
+		int a = (int) (Math.random() * 1000);
+		int b = (int) (Math.random() * 1000);
+		int c = (int) (Math.random() * 1000);
+		int min = -1;
+
+		System.out.println("These are the three randomed numbers: " + a + ", " + b + ", " + c);
+
+		if (a < b) {
+			min = a;
+		} else {
+			min = b;
+		}
+		if (c < min) {
+			min = c;
+		}
+		System.out.println("The smaller number is: " + min);
+
+	}
+}
