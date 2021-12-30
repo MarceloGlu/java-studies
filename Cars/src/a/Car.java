3ddd0b1a60c553a0abf254ef754280b215586e4b
jbt <jbt@405_01>
2021-12-30 09:26:19 +0200

    update

--- Cars/src/a/Car.java
+++ Cars/src/a/Car.java
@@ -5,36 +5,4 @@ public class Car {
 	private int number;		
 	private Speed[] speed = new Speed[0-110];
 	
-	public Car() {
-		super();
-	}
-	
-	public Car(int number, Speed[] speed) {
-		super();
-		this.number = number;
-		this.speed = speed;
-	}
-
-	public int getNumber() {
-		return number;
-	}
-
-	public void setNumber(int number) {
-		this.number = number;
-	}
-
-	public Speed[] getSpeed() {
-		return speed;
-	}
-
-	public void setSpeed(Speed[] speed) {
-		this.speed = speed;
-	}
-
-	
-	
-
-	
-}
-
-	
+		
---
+++ Cars/src/a/Principal.java
@@ -0,0 +1,17 @@
+package a;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Country country = new Country();
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
