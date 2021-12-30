8caaa15cfef64551cb9d32b27f525ed412ee5a8b
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2021-12-30 07:17:21 +0200

    update

--- Cars/src/a/Car.java
+++ Cars/src/a/Car.java
@@ -3,16 +3,32 @@ package a;
 public class Car {
 
 	private int number;		
-	private Speed[] speeds = new Speed[0-110];
-
-	public Car() {
-		super();
-	}
-
-	public Car(int number, Speed[] speeds) {
+	private Speed[] speed = new Speed[0-110];
+	
+	public Car(int number, Speed[] speed) {
 		super();
 		this.number = number;
-		this.speeds = speeds;
+		this.speed = speed;
 	}
 
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Speed[] getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Speed[] speed) {
+		this.speed = speed;
+	}
+	
+
+	
 }
+
+	
