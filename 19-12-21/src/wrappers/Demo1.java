20b630bab48a687ea314528b35786b19f248de64
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2022-01-02 17:07:42 +0200

    update

---
+++ 19-12-21/src/wrappers/Demo1.java
@@ -0,0 +1,14 @@
+package wrappers;
+
+public class Demo1 {
+	
+public static void main(String[] args) {
+	
+	int a = 5;				// este es un primitivo
+	Integer x = 5;			// este es un objeto
+	
+	
+	
+	
+}
+}
