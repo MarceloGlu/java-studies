d880c81d8414360302a7d6fd9adf500cc1469c9b
jbt <jbt@405_01>
2022-01-06 16:01:51 +0200

    proyecto

---
+++ JavaProjectCuponSystem/src/coupon/core/beans/Coupon.java
@@ -0,0 +1,150 @@
+package coupon.core.beans;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Coupon {
+	
+	public enum Category {
+		FOOD, ELECTRICITY, RESTAURANT, VACATON, TRAVEL;
+	}
+	
+	private int id;
+	private int companyId;
+	private String category;
+	private String title;
+	private String description;
+	private LocalDate start_date;
+	private LocalDate end_date;
+	private int amount;
+	private double price;
+	private String image;
+	
+	public Coupon() {
+		super();
+	}
+
+	public Coupon(int id, int companyId, String category, String title, String description, LocalDate start_date,
+			LocalDate end_date, int amount, double price, String image) {
+		super();
+		this.id = id;
+		this.companyId = companyId;
+		this.category = category;
+		this.title = title;
+		this.description = description;
+		this.start_date = start_date;
+		this.end_date = end_date;
+		this.amount = amount;
+		this.price = price;
+		this.image = image;
+	}
+	// Constructors, Getters and Setters, toString, hashcode and equals by id
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public int getCompanyId() {
+		return companyId;
+	}
+
+	public void setCompanyId(int companyId) {
+		this.companyId = companyId;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getStart_date() {
+		return start_date;
+	}
+
+	public void setStart_date(LocalDate start_date) {
+		this.start_date = start_date;
+	}
+
+	public LocalDate getEnd_date() {
+		return end_date;
+	}
+
+	public void setEnd_date(LocalDate end_date) {
+		this.end_date = end_date;
+	}
+
+	public int getAmount() {
+		return amount;
+	}
+
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public String getImage() {
+		return image;
+	}
+
+	public void setImage(String image) {
+		this.image = image;
+	}
+
+	@Override
+	public String toString() {
+		return "Coupon [id=" + id + ", companyId=" + companyId + ", category=" + category + ", title=" + title
+				+ ", description=" + description + ", start_date=" + start_date + ", end_date=" + end_date + ", amount="
+				+ amount + ", price=" + price + ", image=" + image + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Coupon))
+			return false;
+		Coupon other = (Coupon) obj;
+		return id == other.id;
+	}
+	
+	
+}
+
+
+
+
