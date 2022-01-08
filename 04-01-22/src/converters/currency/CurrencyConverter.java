80cb448b3e9daad2f27f6dd703ceac210c973593
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2022-01-08 16:34:56 +0200

    agregado test para converters

--- 04-01-22/src/converters/currency/CurrencyConverter.java
+++ 04-01-22/src/converters/currency/CurrencyConverter.java
@@ -3,11 +3,11 @@ package converters.currency;
 public class CurrencyConverter {
 	
 	public static double convertDolarToNIS(double dollar) {
-		double nis = dollar * 3.09;
+		double nis = dollar * 3.12;
 		return nis;
 	}
 	public static double convertNISToDollar(double nis) {
-		double dollar = nis / 3.09;
+		double dollar = nis / 3.12;
 		return dollar;
 	}
 
