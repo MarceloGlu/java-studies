7b62004ba60c9a2ae38d8010586f3a9751965e0e
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2022-01-08 19:58:38 +0200

    update

--- 04-01-22/src/converters/currency/MyTest.java
+++ 04-01-22/src/converters/currency/MyTest.java
@@ -8,15 +8,19 @@ public class MyTest {
 
 	public static void main(String[] args) {
 		
-		Scanner sc= new Scanner(System.in);    //System.in is a standard input stream  
+		Scanner scanner= new Scanner(System.in);    //System.in is a standard input stream  
 		System.out.print("Enter amount in NIS: ");  
-		double sh= sc.nextDouble();  
+		double sh= scanner.nextDouble(); 
+		
+		
 		
 		double Dollar = CurrencyConverter.convertNISToDollar(sh);
 		
 		System.out.printf("The amount in U$D is: %.2f", Dollar);
 		// the last line prints the amount in dollars limited to 2 digits
 		
+		
+		scanner.close();
 		
 		
 		
