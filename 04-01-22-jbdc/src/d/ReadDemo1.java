6776c112c7b61357b797bc98ee1a681824f06cab
jbt <jbt@405_01>
2022-01-10 19:59:31 +0200

    updated DAO

---
+++ 04-01-22-jbdc/src/d/ReadDemo1.java
@@ -0,0 +1,37 @@
+package d;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ReadDemo1 { 
+
+	public static void main(String[] args) {
+		String dbUrl = "jdbc:mysql://localhost:3306/db2";
+		String user = "root";
+		String password = "1234";
+		
+		
+		
+		try(Connection con = DriverManager.getConnection(dbUrl, user, password)){
+			Statement stmt = con.createStatement();
+			String sql = "select * from person";
+			ResultSet rs = stmt.executeQuery(sql);
+			
+			rs.next(); // move to next row of the result set
+			rs.next(); // move to next row of the result set
+			rs.next(); // move to next row of the result set
+			
+			int id = rs.getInt(1);
+			String name = rs.getString(2);
+
+			System.out.println("id: " + id +", name: " + name);
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	} 
+
+}
