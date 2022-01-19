f5337df83735a8ed7f342b5911ba09e21b94caf8
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2022-01-19 12:53:26 +0200

    trying to solve problems

--- JavaProjectCuponSystem/src/coupon/core/dao/Test.java
+++ JavaProjectCuponSystem/src/coupon/core/dao/Test.java
@@ -7,27 +7,28 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-	
-		
 		try {
-		Company company = new Company(0,"AAA", "AAA@gmail", "aaapassword");
-		CompanyDao companyDao = new CompanyDaoDb();
-		int id = companyDao.addCompany(company);
-		System.out.println("created company: " + id);
-		System.out.println(company);
-					
-		} catch (CouponSystemException e) {
-			e.printStackTrace();
-		}finally {
-			try {
-				ConnectionPool.getInstance().closeAllConnections();
-				
-			}catch (CouponSystemException e) {
+			Company company = new Company(0,"AAA", "AAA@gmail", "aaapassword");
+			CompanyDao companyDao = new CompanyDaoDb();
+			int id = companyDao.addCompany(company);
+			System.out.println("created company: " + id);
+			System.out.println(company);
+						
+			} catch (CouponSystemException e) {
 				e.printStackTrace();
+			}finally {
+				try {
+					ConnectionPool.getInstance().closeAllConnections();
+					
+				}catch (CouponSystemException e) {
+					e.printStackTrace();
+				}
 			}
+			
+			
 		}
-		
-		
+
+
 	}
 
-}
+
