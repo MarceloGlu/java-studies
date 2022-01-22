6b9f27a30c0112e16eefa9560db8fa6d5713a086
MarceloGlu <96018176+MarceloGlu@users.noreply.github.com>
2022-01-22 17:51:43 +0200

    update to copy into new laptop

---
+++ coupon-system-marcelo/src/coupons/core/exception/CouponSystemExceptions.java
@@ -0,0 +1,33 @@
+package coupons.core.exception;
+
+public class CouponSystemExceptions extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public CouponSystemExceptions() {
+		super();
+
+	}
+
+	public CouponSystemExceptions(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+
+	}
+
+	public CouponSystemExceptions(String message, Throwable cause) {
+		super(message, cause);
+
+	}
+
+	public CouponSystemExceptions(String message) {
+		super(message);
+
+	}
+
+	public CouponSystemExceptions(Throwable cause) {
+		super(cause);
+
+	}
+
+}
