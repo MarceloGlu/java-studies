3f444f7b4787edcd2f63c48ff8dae0f50663a400
my pc <my pc@192.168.1.10>
2022-01-23 10:31:01 +0200

    added compay and coupon classes - partially

---
+++ coupon-system-marcelo/src/coupon/core/beans/Company.java
@@ -0,0 +1,12 @@
+package coupon.core.beans;
+
+import java.util.List;
+
+public class Company {
+
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private List<Coupon>coupons; //array-list
+}
---
+++ coupon-system-marcelo/src/coupon/core/beans/Coupon.java
@@ -0,0 +1,20 @@
+package coupon.core.beans;
+
+import java.time.LocalDate;
+
+public class Coupon {
+	
+	private int id;
+	private int companyId;
+	private String category;
+	private String title;
+	private String description;
+	private LocalDate start_date;
+	private LocalDate end_date;
+	private int amount;
+	private double price;
+	private String image;
+	
+	
+
+}
