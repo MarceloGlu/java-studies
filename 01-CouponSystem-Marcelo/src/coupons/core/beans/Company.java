6ba6c8958b372f23217cada3db8452a1822aa8a3
my pc <my pc@192.168.1.10>
2022-01-25 06:16:27 +0200

    building project anew, Company and Coupon classes added

---
+++ 01-CouponSystem-Marcelo/src/coupons/core/beans/Company.java
@@ -0,0 +1,94 @@
+package coupons.core.beans;
+
+import java.util.List;
+import java.util.Objects;
+
+public class Company {
+
+	private int id;
+	private String name;
+	private String email;
+	private String password;
+	private List<Coupon>coupons; // will remain null
+	
+	// constructors, getters/setters, toString, hashCode equals by id
+	
+	public Company() {
+		super();
+	}
+
+	public Company(int id, String name, String email, String password, List<Coupon> coupons) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		this.password = password;
+		this.coupons = coupons;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Coupon> getCoupons() {
+		return coupons;
+	}
+
+	public void setCoupons(List<Coupon> coupons) {
+		this.coupons = coupons;
+	}
+
+	@Override
+	public String toString() {
+		return "Company [id=" + id + ", name=" + name + ", email=" + email + ", password=" + password + ", coupons="
+				+ coupons + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!(obj instanceof Company))
+			return false;
+		Company other = (Company) obj;
+		return id == other.id;
+	}
+	
+	
+	
+	
+	
+}
