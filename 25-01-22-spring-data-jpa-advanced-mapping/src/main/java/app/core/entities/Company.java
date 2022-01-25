d396b41e8ebdd3fd99f1159949104bf4e2798454
my pc <my pc@192.168.1.10>
2022-01-25 13:57:25 +0200

    update

---
+++ 25-01-22-spring-data-jpa-advanced-mapping/src/main/java/app/core/entities/Company.java
@@ -0,0 +1,38 @@
+package app.core.entities;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+/*
+ * spring beans (@Component, @Bean, are managed by spring context (pring container)
+ * JPA Entities (@Entity) are managed by Hibernate session context (hibdernate session)
+ * Entity is not component. it is not managed by spring context.
+ * this is Entity. managed by hibernate.
+ * */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity // 
+public class Company {
+
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String name;
+	private String email;
+	@OneToOne(cascade = CascadeType.ALL)
+	@JoinColumn(name = "address_id")	
+	private CompanyAddress address;
+}
---
+++ 25-01-22-spring-data-jpa-advanced-mapping/src/main/java/app/core/entities/CompanyAddress.java
@@ -0,0 +1,36 @@
+package app.core.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+
+/*
+ * spring beans (@Component, @Bean, are managed by spring context (pring container)
+ * JPA Entities (@Entity) are managed by Hibernate session context (hibdernate session)
+ * Entity is not component. it is not managed by spring context.
+ * this is Entity. managed by hibernate.
+ * */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class CompanyAddress {
+
+	@Id // primary key
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String country;
+	private String city;
+	private String street;
+	@Column(name = "apt_number") // give column name in the database
+	private int aptNumber;
+}
