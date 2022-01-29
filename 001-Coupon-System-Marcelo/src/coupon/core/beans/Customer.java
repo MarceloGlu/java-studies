2e1af037a0f2a6a4723acc9e0b0e017ba5ca752e
my pc <my pc@192.168.1.10>
2022-01-29 15:50:44 +0200

    update

--- 001-Coupon-System-Marcelo/src/coupon/core/beans/Customer.java
+++ 001-Coupon-System-Marcelo/src/coupon/core/beans/Customer.java
@@ -1,5 +1,8 @@
 package coupon.core.beans;
 
+import java.util.List;
+import java.util.Objects;
+
 public class Customer {
 	
 	private int id;
@@ -7,8 +10,94 @@ public class Customer {
 	private String lastName;
 	private String email;
 	private String password;
+	private List<Coupon>coupons; //will remain null
 	
+	// add: CTORS, get/set, toString, hashCode = equals(by id)
 	
-	
+	public Customer() {
+		super();
+	}
 
+	public Customer(int id, String firstName, String lastName, String email, String password, List<Coupon> coupons) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.password = password;
+		this.coupons = coupons;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public List<Coupon> getCoupons() {
+		return coupons;
+	}
+
+	public void setCoupons(List<Coupon> coupons) {
+		this.coupons = coupons;
+	}
+
+	@Override
+	public String toString() {
+		return "Customer [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email
+				+ ", password=" + password + ", coupons=" + coupons + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Customer other = (Customer) obj;
+		return id == other.id;
+	}
+
+	
 }
